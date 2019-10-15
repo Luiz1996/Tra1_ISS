7bc332afdfd9875a015f0818808820707170bbe9
Luiz Flávio <luiz_flavio_p@hotmail.com>
2019-10-15 19:37:59 -0300

    Iniciado a criação da tela de cadastramento de cotação.

---
+++ src/main/java/br/uem/din/bibliotec/config/controller/CotacaoController.java
@@ -0,0 +1,45 @@
+package br.uem.din.bibliotec.config.controller;
+
+import br.uem.din.bibliotec.config.dao.CotacaoDao;
+import br.uem.din.bibliotec.config.model.Cotacao;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class CotacaoController implements Serializable {
+    private Cotacao cotacao = new Cotacao();
+    private CotacaoDao cotacaoDao = new CotacaoDao();
+
+    public CotacaoController(){}
+
+    public Cotacao getCotacao() {
+        return cotacao;
+    }
+
+    public void setCotacao(Cotacao cotacao) {
+        this.cotacao = cotacao;
+    }
+
+    public CotacaoDao getCotacaoDao() {
+        return cotacaoDao;
+    }
+
+    public void setCotacaoDao(CotacaoDao cotacaoDao) {
+        this.cotacaoDao = cotacaoDao;
+    }
+
+    public String cadastrarCotacao(){
+        if(cotacao.getValor() <= 0.0){
+            cotacao.setMsgRetorno("FALHA: Valor de cotação inválido(inserir valores maiores que R$0,00).");
+            cotacao.setColorMsgRetorno("red");
+        }else{
+            cotacaoDao.cadastrarCotacao(cotacao);
+            cotacao.setMsgRetorno("SUCESSO: A cotação de R$"+cotacao.getValor()+" foi cadastrada com sucesso.");
+            cotacao.setColorMsgRetorno("green");
+        }
+        return "/acessoBalconista?faces-redirect=true";
+    }
+}
---
+++ src/main/java/br/uem/din/bibliotec/config/dao/CotacaoDao.java
@@ -0,0 +1,9 @@
+package br.uem.din.bibliotec.config.dao;
+
+import br.uem.din.bibliotec.config.model.Cotacao;
+
+public class CotacaoDao {
+    public void cadastrarCotacao(Cotacao cotacao) {
+        System.out.println("fazer insert aqui");
+    }
+}
