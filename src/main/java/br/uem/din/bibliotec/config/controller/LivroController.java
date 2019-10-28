6618e56ab24f76d1217c9e0005f22aaacfc9890d
William D. Costa <wdouglascosta@outlook.com>
2019-10-28 20:54:32 -0300

    controlador Livro

--- src/main/java/br/uem/din/bibliotec/config/controller/LivroController.java
+++ src/main/java/br/uem/din/bibliotec/config/controller/LivroController.java
@@ -1,9 +1,10 @@
+
+
 package br.uem.din.bibliotec.config.controller;
 
+import br.uem.din.bibliotec.config.dao.LivroDao;
 import br.uem.din.bibliotec.config.dao.UsuarioDao;
 import br.uem.din.bibliotec.config.model.Livro;
-import br.uem.din.bibliotec.config.dao.LivroDao;
-import org.primefaces.context.PrimeFacesContext;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -30,44 +31,70 @@ public class LivroController implements Serializable {
         this.livro = livro;
     }
 
-    public LivroDao getLivroDAO() { return livroDao; }
+    public LivroDao getLivroDAO() {
+        return livroDao;
+    }
 
     public void setLivroDAO(LivroDao livroDAO) {
         this.livroDao = livroDAO;
     }
 
-    public UsuarioDao getUserDAO() { return userDao; }
+    public UsuarioDao getUserDAO() {
+        return userDao;
+    }
 
-    public void setUserDAO(UsuarioDao userDAO) { this.userDao = userDAO; }
+    public void setUserDAO(UsuarioDao userDAO) {
+        this.userDao = userDAO;
+    }
 
-    public String getSUCESSO() { return SUCESSO; }
+    public String getSUCESSO() {
+        return SUCESSO;
+    }
 
-    public String getFALHA() { return FALHA; }
+    public String getFALHA() {
+        return FALHA;
+    }
 
-    public LivroDao getLivroDao() { return livroDao; }
+    public LivroDao getLivroDao() {
+        return livroDao;
+    }
 
-    public void setLivroDao(LivroDao livroDao) { this.livroDao = livroDao; }
+    public void setLivroDao(LivroDao livroDao) {
+        this.livroDao = livroDao;
+    }
 
-    public UsuarioDao getUserDao() { return userDao; }
+    public UsuarioDao getUserDao() {
+        return userDao;
+    }
 
-    public void setUserDao(UsuarioDao userDao) { this.userDao = userDao; }
+    public void setUserDao(UsuarioDao userDao) {
+        this.userDao = userDao;
+    }
 
-    public int getRetorno() { return retorno; }
+    public int getRetorno() {
+        return retorno;
+    }
 
-    public void setRetorno(int retorno) { this.retorno = retorno; }
+    public void setRetorno(int retorno) {
+        this.retorno = retorno;
+    }
 
     //chama método pra cadastramento de livro(s)
     public String realizarCadastroLivro() throws SQLException {
         retorno = livroDao.cadastrarLivro(livro);
 
-        if(retorno == 1){
+        if (retorno == 1) {
             livro.setMsg_retorno("Retorno: O livro foi cadastrado com sucesso.");
             livro.setColor_msg_retorno(SUCESSO);
-        }else{
+        } else {
             livro.setMsg_retorno("Retorno: A operação de cadastramento do livro falhou.");
             livro.setColor_msg_retorno(FALHA);
         }
         return userDao.homePage();
+    }
+
+    public List<Livro> livrosDisponiveis() throws SQLException {
+        return livroDao.consultaLivrosDisponiveis(livro);
     }
 
     //chama método para consultar livro(s)
@@ -84,104 +111,48 @@ public class LivroController implements Serializable {
     }
 
     //chama método para exclusão de livro (exclusão lógica, ativo = 0)
-    public String realizarDelecaoLivro(){
-        retorno =  livroDao.deletarLivro(livro);
+    public String realizarDelecaoLivro() {
+        retorno = livroDao.deletarLivro(livro);
 
-        if(retorno == 0){
-            livro.setMsg_retorno("Retorno: A deleção do livro falhou.");
-            livro.setColor_msg_retorno(FALHA);
-        }else{
-            if(retorno == 1){
+        switch (retorno) {
+            case 0:
+                livro.setMsg_retorno("Retorno: A deleção do livro falhou.");
+                livro.setColor_msg_retorno(FALHA);
+                break;
+            case 1:
                 livro.setMsg_retorno("Retorno: O livro foi deletado com sucesso.");
                 livro.setColor_msg_retorno(SUCESSO);
-            }else{
-                if(retorno == -1){
-                    livro.setMsg_retorno("Retorno: O livro possui reserva, deleção falhou.");
-                    livro.setColor_msg_retorno(FALHA);
-                }else{
-                    if(retorno == -2){
-                        livro.setMsg_retorno("Retorno: O livro possui empréstimos, deleção falhou.");
-                        livro.setColor_msg_retorno(FALHA);
-                    }
-                }
-            }
+                break;
+            case -1:
+                livro.setMsg_retorno("Retorno: O livro possui reserva, deleção falhou.");
+                livro.setColor_msg_retorno(FALHA);
+                break;
+            case -2:
+                livro.setMsg_retorno("Retorno: O livro possui empréstimos, deleção falhou.");
+                livro.setColor_msg_retorno(FALHA);
+                break;
         }
+
+
         return userDao.homePage();
     }
 
     //chama método para editar as informações de um livro
-    public String realizarEdicaoLivro(){
+    public String realizarEdicaoLivro() {
         retorno = livroDao.editarLivro(livro);
 
-        if(retorno == 1){
+        if (retorno == 1) {
             livro.setMsg_retorno("Retorno: As informações do livro foram atualizadas com sucesso.");
             livro.setColor_msg_retorno(SUCESSO);
-        }else{
-            if(retorno == 0){
+        } else {
+            if (retorno == 0) {
                 livro.setMsg_retorno("Retorno: A operação de alteração do livro selecionado falhou.");
                 livro.setColor_msg_retorno(FALHA);
-            }else{
+            } else {
                 livro.setMsg_retorno("Retorno: O livro informado não existe.");
                 livro.setColor_msg_retorno(FALHA);
             }
         }
         return userDao.homePage();
-    }
-
-    //chama métodos para manipulação da reserva
-    public String realizaReservaLivro(){
-        retorno =  livroDao.cadastrarReserva(livro);
-
-        if(retorno == -2){
-            livro.setMsg_retorno("Retorno: Você possui empréstimos em atraso, favor regularizar sua situação.");
-            livro.setColor_msg_retorno(FALHA);
-        }else{
-            if(retorno == -1){
-                livro.setMsg_retorno("Retorno: O cadastramento de reserva falhou, contacte o administrador!");
-                livro.setColor_msg_retorno(FALHA);
-            }else{
-                if(retorno == 0){
-                    livro.setMsg_retorno("Retorno: Reserva efetuada com sucesso, consulte a data de retirada clicando em 'Minhas Reservas'.");
-                    livro.setColor_msg_retorno(SUCESSO);
-                }else{
-                    if(retorno == 1){
-                        livro.setMsg_retorno("Retorno: O livro selecionado já possui reserva em seu nome, consulte suas reservas clicando em 'Minhas Reservas'.");
-                        livro.setColor_msg_retorno(FALHA);
-                    }else{
-                        if(retorno == 2){
-                            livro.setMsg_retorno("Retorno: O livro selecionado já possui reserva em nome de outra pessoa.");
-                            livro.setColor_msg_retorno(FALHA);
-                        }else{
-                            if(retorno == 3){
-                                livro.setMsg_retorno("Retorno: O livro já encontra-se com empréstimo em seu nome!");
-                                livro.setColor_msg_retorno(FALHA);
-                            }else{
-                                livro.setMsg_retorno("Retorno: A operação de cadastramento de reserva falhou.");
-                                livro.setColor_msg_retorno(FALHA);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return userDao.homePage();
-    }
-
-    public String realizaCancelamentoReserva(){
-        retorno =  livroDao.cancelarReserva(livro);
-
-        if(retorno == 0){
-            livro.setMsg_retorno("Retorno: A operação de cancelamento de reserva falhou.");
-            livro.setColor_msg_retorno(FALHA);
-        }else{
-            livro.setMsg_retorno("Retorno: Reserva cancelada com sucesso.");
-            livro.setColor_msg_retorno(SUCESSO);
-        }
-        return userDao.homePage();
-    }
-
-    public void carregarDadosLivro() {
-        livroDao.carregarDadosLivro(livro);
     }
 }
