7b8c9af52a6f840fa638496bf94b15875ac8be37
Marcos <mvperes95@gmail.com>
2019-11-26 09:19:33 -0300

    Teste da MultaDao com Junity
    em Andamento;

---
+++ teste/br/uem/din/bibliotec/config/dao/MultaDaoTest.java
@@ -0,0 +1,26 @@
+package br.uem.din.bibliotec.config.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultaDaoTest {
+
+    @org.junit.jupiter.api.Test
+    void cadastrarMulta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void valorTotalMultas() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consultarMultasCpf() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void baixarMultas() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consultarMinhasMultasCpf() {
+    }
+}
